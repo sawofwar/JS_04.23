a172814525b0e6cef9402415021b6d85f6219d78
ConstantineF <warsawxx@gmail.com>
2023-04-08 02:02:54 +0400

    created object for testing and testCopy variable

--- index.js
+++ index.js
@@ -7,4 +7,31 @@ function makeDeepCopy(object) {
   return Object.assign({}, object);
 }
 
-makeDeepCopy();
+const testObject = {
+  a: 123,
+  b: {
+    c: [1, 2, 3],
+    d: {
+      e: "test",
+      f: {
+        g: "test string",
+        h: [
+          1,
+          2,
+          3,
+          {
+            i: "test string 2",
+            j: true,
+          },
+        ],
+      },
+    },
+  },
+};
+
+const testCopy = makeDeepCopy(testObject);
+console.log(testCopy);
+
+testCopy.b.d.e = "rewritten";
+console.log(testCopy.b.d.e);
+console.log(testObject.b.d.e);
