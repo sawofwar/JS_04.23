f91286c2aeb69dd89ac2e7afa68a6c1a5d60d95e
ConstantineF <warsawxx@gmail.com>
2023-04-09 23:32:11 +0400

    if statement checks for instanceof Map first, copies maps too

--- index.js
+++ index.js
@@ -14,8 +14,19 @@ function makeDeepCopy(object) {
   // 1. accept object, return copy
 
   const clone = {};
+
+  // looping through object props
   for (const property in object) {
-    if (typeof object[property] === "object" && object[property] !== null) {
+    // if property is object & if it's not null, do a recursion
+
+    // console.log("Property is object : ", object[property]);
+
+    if (object[property] instanceof Map) {
+      clone[property] = new Map(object[property]);
+    } else if (
+      typeof object[property] === "object" &&
+      object[property] !== null
+    ) {
       clone[property] = makeDeepCopy(object[property]);
     } else {
       clone[property] = object[property];
@@ -26,74 +37,16 @@ function makeDeepCopy(object) {
 }
 
 const testObject = {
-  a: 123,
+  a: true,
   b: {
-    c: [1, 2, 3],
-    d: {
-      e: "test",
-      f: {
-        g: "test string",
-        h: [
-          1,
-          2,
-          3,
-          {
-            i: "test string 2",
-            j: true,
-          },
-        ],
-      },
-    },
+    c: new Map([
+      [1, true],
+      [[2, 3, 4], "test string"],
+    ]),
   },
 };
 
-const testObject2 = {
-  a: "b",
-  b: {
-    d: "e",
-    c: false,
-  },
-};
+const copy = makeDeepCopy(testObject);
 
-const testObject3 = {
-  a: "b",
-  b: {
-    c: false,
-    d: {
-      e: "test string",
-      f: 123,
-    },
-  },
-};
-
-// array for testing
-const testArray = [1, , "abc", [true, { a: 99, b: "hello" }]];
-
-const testMap = new Map([
-  ["test", true],
-  ["b", 123],
-  [
-    "testObject",
-    {
-      a: "j",
-      b: 2394,
-      c: false,
-    },
-  ],
-]);
-
-const testCopy = makeDeepCopy(testObject);
-const testCopyMap = makeDeepCopy(testArray);
-
-const testArrayCopy = makeDeepCopy(testArray);
-console.log(testArrayCopy);
-
-testArrayCopy[3][1].b = "jsjsj";
-
-// testArray[3][1].b = "jsjsj";
-console.log(`Original array prop: ${testArray[3][1].b}`);
-console.log(`Copy array prop rewritten: ${testArrayCopy[3][1].b}`);
-
-testCopy.b.d.e = "rewritten";
-console.log(testObject.b.d.e);
-console.log(testCopy.b.d.e);
+console.log("Copy : ", copy.b);
+console.log("Original object : ", testObject.b);
