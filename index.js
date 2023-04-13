6ff632a441222f7e7b4e416b1f9eec9df38405bf
ConstantineF <warsawxx@gmail.com>
2023-04-13 18:24:54 +0400

    [3.2:04.13] finished second task

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 "use strict";
 
 // 🔥 TASK 1
-// 🔥 TODO: ask if logging this should happen on each iteration or just once
-// TODO: ask if it's okay if arrow function points to window
 Array.prototype.customFilter = function (callbackFn, thisObject) {
   const filteredArray = [];
 
@@ -18,22 +16,46 @@ Array.prototype.customFilter = function (callbackFn, thisObject) {
   return filteredArray;
 };
 
-// 🔥 random tests
+// 🔥 TASK 2
+/*
+1st call: counter = 1000 --> alreadyCalled is false --> timer started
+2nd call: counter = 1000 --> alreadyCalled is true --> timer cancelled
+*/
+
+function createDebounceFunction(callbackFn, delay) {
+  let timer;
+  let isAlreadyCalled = true;
+
+  return function () {
+    timer = setTimeout(callbackFn, delay);
+    isAlreadyCalled && clearTimeout(timer);
+
+    isAlreadyCalled &&= false;
+  };
+}
+
 /*
 const testFunction = function () {
-  console.log("hi");
+  console.log("hi from test function");
 };
 
-const callbackFunction = function (element, index, array) {
-  console.log("hi");
-};
+const testFunctionDebounce = createDebounceFunction(testFunction, 1000); // 1 sec
 
-const testArray = [1, 2, 3, 4, 5];
+testFunctionDebounce();
+testFunctionDebounce();
+setTimeout(testFunctionDebounce, 5000); // 3 sec
 
-const filteredTestArray = testArray.customFilter(testFunction, {
-  name: "constantine",
-  age: 27,
-  hasGf: true,
-});
-console.log(filteredTestArray);
+// ??? called after 4 sec
+
+console.log("0 seconds");
+setTimeout(() => console.log("1 second"), 999);
+setTimeout(() => console.log("2 seconds"), 1999);
+setTimeout(() => console.log("3 seconds"), 2999);
+setTimeout(() => console.log("4 seconds"), 3999);
+setTimeout(() => console.log("5 seconds"), 4999);
+setTimeout(() => console.log("6 seconds"), 5999);
+setTimeout(() => console.log("7 seconds"), 6999);
+setTimeout(() => console.log("8 seconds"), 7999);
+setTimeout(() => console.log("9 seconds"), 8999);
+setTimeout(() => console.log("10 seconds"), 9999);
 */
