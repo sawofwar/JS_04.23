69f5c77c0e5c546cebba8787376afa97eaf712a8
ConstantineF <warsawxx@gmail.com>
2023-04-13 18:25:27 +0400

    both tasks finished, cleaned up the code

--- index.js
+++ index.js
@@ -17,11 +17,6 @@ Array.prototype.customFilter = function (callbackFn, thisObject) {
 };
 
 // 🔥 TASK 2
-/*
-1st call: counter = 1000 --> alreadyCalled is false --> timer started
-2nd call: counter = 1000 --> alreadyCalled is true --> timer cancelled
-*/
-
 function createDebounceFunction(callbackFn, delay) {
   let timer;
   let isAlreadyCalled = true;
@@ -33,29 +28,3 @@ function createDebounceFunction(callbackFn, delay) {
     isAlreadyCalled &&= false;
   };
 }
-
-/*
-const testFunction = function () {
-  console.log("hi from test function");
-};
-
-const testFunctionDebounce = createDebounceFunction(testFunction, 1000); // 1 sec
-
-testFunctionDebounce();
-testFunctionDebounce();
-setTimeout(testFunctionDebounce, 5000); // 3 sec
-
-// ??? called after 4 sec
-
-console.log("0 seconds");
-setTimeout(() => console.log("1 second"), 999);
-setTimeout(() => console.log("2 seconds"), 1999);
-setTimeout(() => console.log("3 seconds"), 2999);
-setTimeout(() => console.log("4 seconds"), 3999);
-setTimeout(() => console.log("5 seconds"), 4999);
-setTimeout(() => console.log("6 seconds"), 5999);
-setTimeout(() => console.log("7 seconds"), 6999);
-setTimeout(() => console.log("8 seconds"), 7999);
-setTimeout(() => console.log("9 seconds"), 8999);
-setTimeout(() => console.log("10 seconds"), 9999);
-*/
