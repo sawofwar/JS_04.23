b4536570087ff6025006567ee6e3c7787dda95e0
ConstantineF <warsawxx@gmail.com>
2023-04-16 19:43:21 +0400

    second task calculator class with error checks

--- index.js
+++ index.js
@@ -17,3 +17,100 @@ function curry(fn) {
     }
   };
 }
+
+// 🔥 TASK 2
+
+/*
+Calculator class
+
+constructor accepts two valid numbers (integers & floats);
+if one of the parameters is invalid, throw new Error
+
+class has methods:
+setX
+setY
+getSum
+getMul
+getSub
+getDiv
+
+
+setX -- first number
+*/
+
+class Calculator {
+  constructor(x, y) {
+    // check number
+    if (typeof x !== "number" || typeof y !== "number") throw new Error();
+    // check infinity
+    if (Math.abs(x) === Infinity || Math.abs(y) === Infinity) throw new Error();
+    // check NaN
+    if (isNaN(x) || isNaN(y)) throw new Error();
+
+    this.x = x;
+    this.y = y;
+  }
+
+  setX(num) {
+    // check number
+    if (typeof num !== "number") throw new Error();
+    // check infinity
+    if (Math.abs(num) === Infinity) throw new Error();
+    // check NaN
+    if (isNaN(num)) throw new Error();
+
+    this.x = num;
+  }
+  setY(num) {
+    // check number
+    if (typeof num !== "number") throw new Error();
+    // check infinity
+    if (Math.abs(num) === Infinity) throw new Error();
+    // check NaN
+    if (isNaN(num)) throw new Error();
+
+    this.y = num;
+  }
+
+  getSum() {
+    return this.x + this.y;
+  }
+
+  getMul() {
+    return this.x * this.y;
+  }
+
+  getSub() {
+    return Math.abs(this.x - this.y);
+  }
+
+  getDiv() {
+    if (this.y == 0) throw new Error();
+
+    return this.x / this.y;
+  }
+}
+
+const myCalculator = new Calculator(10, 10);
+console.log(myCalculator.setX(10));
+console.log(myCalculator.x);
+
+// myCalculator.setX();
+// console.log(!!0);
+
+/*
+string
+number
+boolean
+undefined
+null
+bigInt
+symbsols
+
+array
+object
+function/class
+map
+set
+
+*/
