64305d52e045e66cff5606b9b284ab2241d481e7
ConstantineF <warsawxx@gmail.com>
2023-04-18 12:43:38 +0400

    init commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 "use strict";
+
+class Stack {
+  constructor(stackSize) {
+    // if (typeof stackSize !== "number") throw new Error("123");
+    // if (stackSize === Infinity || -Infinity) throw new Error();
+    // if (isNaN(stackSize)) throw new Error();
+    // this.stackSize = stackSize;
+  }
+}
+
+function newStack(number) {
+  const stack = new Stack(number);
+}
+
+const myStack = new Stack();
+
+// module.exports = Stack;
+// module.exports = newStack;
+// module.exports = myStack;
+module.exports = [Stack, newStack, myStack];
